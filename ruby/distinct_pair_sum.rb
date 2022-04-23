8c45e9194aa4803d9bd3a473d523faa764fe961c
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-23 15:47:31 -0700

    finish solution

--- ruby/distinct_pair_sum.rb
+++ ruby/distinct_pair_sum.rb
@@ -1,5 +1,16 @@
 def distinct_pair_sum(arr, k)
-  # type your code in here
+  ans = []
+  i = 0
+  (arr.length-1).times do
+    if k == arr[i] + arr[i+1]
+      new = [arr[i], arr[i+1]]
+      if not ans.include?(new) || ans.include?(new.reverse)
+        ans.push(new)
+      end
+      end
+      i += 1
+  end
+  ans
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -17,4 +28,12 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+
+# iterate through array
+# check pairs of numbers and see if they add up to k
+# if yes, check if answer array has pair or reverse
+#   if it doesn't, push it to ans array
+
+
+
 # And a written explanation of your solution
